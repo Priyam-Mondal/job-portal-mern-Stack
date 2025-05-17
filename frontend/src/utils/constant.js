bb3a283fa80cb46789e6a4d2d40ef530fa02503e
Priyam Mondal <mondalpriyam99@gmail.com>
2025-05-17 09:38:56 +0530

    change env in frontend

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,7 +1,14 @@
-export const VITE_REACT__APP_BACKEND_BASEURL = "http://localhost:3000/";
+const VITE_REACT_APP_BACKEND_BASEURL = import.meta.env
+  .VITE_REACT_APP_BACKEND_BASEURL;
 
-export const USER_API_END_POINT = `${VITE_REACT__APP_BACKEND_BASEURL}api/v1/user`;
-export const JOB_API_END_POINT = `${VITE_REACT__APP_BACKEND_BASEURL}api/v1/job`;
-export const APPLICATION_API_END_POINT = `${VITE_REACT__APP_BACKEND_BASEURL}api/v1/application`;
-export const COMPANY_API_END_POINT = `${VITE_REACT__APP_BACKEND_BASEURL}api/v1/company`;
-export const RESUME_API_END_POINT = `${VITE_REACT__APP_BACKEND_BASEURL}api/v1/resumebuilder`;
+if (!VITE_REACT_APP_BACKEND_BASEURL) {
+  console.error(
+    "VITE_REACT_APP_BACKEND_BASEURL is not defined. Make sure it's in your .env file and starts with VITE_"
+  );
+}
+
+export const USER_API_END_POINT = `${VITE_REACT_APP_BACKEND_BASEURL}api/v1/user`;
+export const JOB_API_END_POINT = `${VITE_REACT_APP_BACKEND_BASEURL}api/v1/job`;
+export const APPLICATION_API_END_POINT = `${VITE_REACT_APP_BACKEND_BASEURL}api/v1/application`;
+export const COMPANY_API_END_POINT = `${VITE_REACT_APP_BACKEND_BASEURL}api/v1/company`;
+export const RESUME_API_END_POINT = `${VITE_REACT_APP_BACKEND_BASEURL}api/v1/resumebuilder`;
